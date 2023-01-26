b951bdde05452079f698aa6eb2fa262302b7af87
Oliwier Mazurek <o.mazurek@technikumczestochowa.spsk.pl>
2023-01-26 16:37:05 +0100

    Dodanie górnej części oraz wgranie font awesome

--- assets/react/controllers/Header.jsx
+++ assets/react/controllers/Header.jsx
@@ -1,18 +1,26 @@
 import React from 'react';
+import { BiDownArrowAlt } from "react-icons/bi"
+
 
 export default function (props) {
     return <div className="main-container">
         <div className="header-container">
             <div className='header-text'>
-            <p>
+            <h1>
                  As a digital consultancy company, we deliver
                  individual, tailored online marketing solutions to generate growth.
-            </p>
-            <p>
+            </h1>
+            <h1>
             If you are thinking about combining media, data
                  and technology in order to make better business decisions, you have come to the right place.
-            </p>
+            </h1>
             </div>
+
+            
+                <button className='top-button'>ASK FOR DETAILS</button><br></br>
+                <BiDownArrowAlt className='fa-i'/>
+                
+              
         </div>
     </div>
 
